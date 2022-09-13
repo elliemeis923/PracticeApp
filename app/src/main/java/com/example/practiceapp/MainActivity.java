a4700ecffc9f285aeefcba242ddeeafe61798267
elliemeis923 <elliemeis923@gmail.com>
2022-09-13 13:18:00 -0500

    i have java code, i just need to fix xml format

--- app/src/main/java/com/example/practiceapp/MainActivity.java
+++ app/src/main/java/com/example/practiceapp/MainActivity.java
@@ -1,10 +1,10 @@
+package com.example.practiceapp;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.practiceapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,26 +13,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     /**
      * Increase the score for Team A by 1 point.
      */
     public void addOneForTeamA(View view) {
-        displayForTeamA(1);
+        scoreTeamA ++;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
      * Increase the score for Team A by 2 points.
      */
     public void addTwoForTeamA(View view) {
-        displayForTeamA(2);
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
      * Increase the score for Team A by 3 points..
      */
     public void addThreeForTeamA(View view) {
-        displayForTeamA(3);
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addOneForTeamB(View view) {
+        scoreTeamB ++;
+        displayForTeamA(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamA(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points..
+     */
+    public void addThreeForTeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamA(scoreTeamB);
+    }
+
+    public void reset(View view){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 
     /**
@@ -40,6 +73,11 @@ public class MainActivity extends AppCompatActivity {
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 }
