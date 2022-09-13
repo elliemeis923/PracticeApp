34d1579a56d36f8a3b14201ee6b936a30988abdf
elliemeis923 <elliemeis923@gmail.com>
2022-09-13 11:13:06 -0500

    initial commit

--- app/src/main/java/com/example/practiceapp/MainActivity.java
+++ app/src/main/java/com/example/practiceapp/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.practiceapp;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.practiceapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public void addOneForTeamA(View view) {
+        displayForTeamA(1);
+    }
+
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View view) {
+        displayForTeamA(2);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points..
+     */
+    public void addThreeForTeamA(View view) {
+        displayForTeamA(3);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
